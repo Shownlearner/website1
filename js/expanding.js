7946a0e6f3f1b2f3199421da8ebd36c3cedc2510
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-02-28 18:12:47 +0000

    Fri, Feb 28, 2025, 10:12 AM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -6,6 +6,7 @@ const panels = document.querySelectorAll('.panel')
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses
+        panel.classList.add('active')
     })
 })
 
