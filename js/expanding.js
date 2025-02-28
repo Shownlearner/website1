18cf70f21dcc74413af3aeea5f9485a5d46a05dc
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-02-28 18:09:02 +0000

    Fri, Feb 28, 2025, 10:09 AM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -1,4 +1,14 @@
 const panels = document.querySelectorAll('.panel')
 
-console.log("hello")
-console.log(panels)
+//console.log("hello")
+//console.log(panels) un comment if you want to see what they do
+
+panels.forEach(panel => {
+    removeActiveClasses()
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
