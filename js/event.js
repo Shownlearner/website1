089f1bf8f41e88633839fb88fe901acd4d0f37e2
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-03-04 18:09:41 +0000

    Tue, Mar 4, 2025, 10:09 AM -08:00

--- js/event.js
+++ js/event.js
@@ -61,6 +61,7 @@ document.addEventListener('keyup', (event)=>{
         element4.style.opacity = 1
         element4.style.width = "300px"
         element4.style.height = "50px"
+        element4.innerHTML = "<h4>Click me to make me small <br> press 'j' to bring me back.</h4>"
     }
 })
 =
