d90103d884bdcf94064b0656c4b6c1dab64c01b0
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-03-19 16:56:17 +0000

    Wed, Mar 19, 2025, 9:56 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -3,7 +3,7 @@ const start_btn = document.getElementById('start-btn')
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
 const game_container = document.getElementById('game-container')
 const timeEl = document.getElementById('time')
-const scoreEl = docuemnt.getElementById('score')
+const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
 
 start_btn.addEventListener('click', ()=>{
