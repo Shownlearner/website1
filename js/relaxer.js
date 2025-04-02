8fa7cd7a8e267c659802b2a11e71a1ed4cf4f7c6
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-04-02 15:56:43 +0000

    Wed, Apr 2, 2025, 8:56 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 
+breatheAnimation();
 setInterval(breatheAnimation, 7500);
 
 function breatheAnimation() {
