495c0c7ce94f4ecf921b719d0f65b856d229362e
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-05-27 16:45:41 +0000

    Tue, May 27, 2025, 9:45 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,15 @@
 const upButton = document.querySelector('.up-button');
 const downButton = document.querySelector('.down-button');
 const rightSide = document.querySelector('.right-slide');
+const leftSide = document.querySelector('.left-slide');
 const slideslength = rightSide.querySelectorAll('div').length;
+
+
+upButton.addEventListener('click', () => {
+    changeSlide('up');
+}
+);
+downButton.addEventListener('click', () => {
+    changeSlide('down');
+}
+);
