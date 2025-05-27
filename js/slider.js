3a60c89cd833b3fba20ff03ece4b214b80f8b566
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-05-27 17:10:16 +0000

    Tue, May 27, 2025, 10:10 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -17,10 +17,24 @@ function changeSlide(direction) {
     const rightSlide = rightSide.querySelector('.right');
 
     if (direction === 'up') {
+        if (currentSlide >= slideslength) {
+            currentSlide = 1; // Reset to first slide
+        }
+        if (currentSlide <= 1) {
+            currentSlide = slideslength; // Reset to last slide
+        }
+
         currentSlide++;
         leftSide.style.top = `${-(slideslength - currentSlide) * 100}%`;
         rightSide.style.transform = `translateY(-${(currentSlide - 1) * 100}%)`;
     } else if (direction === 'down') {
+        if (currentSlide >= slideslength) {
+            currentSlide = 1; // Reset to first slide
+        }
+        if (currentSlide <= 1) {
+            currentSlide = slideslength; // Reset to last slide
+        }
+
         currentSlide--;
         leftSide.style.top = `${-(slideslength - currentSlide) * 100}%`;
         rightSide.style.transform = `translateY(-${(currentSlide - 1) * 100}%)`;
