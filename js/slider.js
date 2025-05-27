b788db9b73644fd43b7a3d380243b7de465cebd8
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-05-27 17:06:15 +0000

    Tue, May 27, 2025, 10:06 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -19,6 +19,7 @@ function changeSlide(direction) {
     if (direction === 'up') {
         currentSlide++;
         leftSide.style.top = `${-(slideslength - currentSlide) * 100}%`;
+        rightSide.style.transform = `translateY(-${(currentSlide - 1) * 100}%)`;
     } else if (direction === 'down') {
         currentSlide--;
         leftSide.style.top = `${-(slideslength - currentSlide) * 100}%`;
