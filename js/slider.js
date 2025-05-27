447538e0084644f487a636d1b3c1aaa4b6a2c55b
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-05-27 16:47:22 +0000

    Tue, May 27, 2025, 9:47 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -5,11 +5,25 @@ const leftSide = document.querySelector('.left-slide');
 const slideslength = rightSide.querySelectorAll('div').length;
 
 
-upButton.addEventListener('click', () => {
-    changeSlide('up');
+upButton.addEventListener('click', () => { changeSlide('up'); } )
+downButton.addEventListener('click', () => { changeSlide('down'); } )
+
+function changeSlide(direction) {
+    const topSlide = leftSide.querySelector('.top');
+    const bottomSlide = leftSide.querySelector('.bottom');
+    const rightSlide = rightSide.querySelector('.right');
+
+    if (direction === 'up') {
+        topSlide.classList.remove('top');
+        topSlide.classList.add('bottom');
+        bottomSlide.classList.remove('bottom');
+        bottomSlide.classList.add('top');
+        rightSide.appendChild(rightSlide);
+    } else if (direction === 'down') {
+        topSlide.classList.remove('top');
+        topSlide.classList.add('bottom');
+        bottomSlide.classList.remove('bottom');
+        bottomSlide.classList.add('top');
+        rightSide.prepend(rightSlide);
+    }
 }
-);
-downButton.addEventListener('click', () => {
-    changeSlide('down');
-}
-);
