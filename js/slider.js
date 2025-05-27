e7d1a4a387c2337800d63fcc502cd94d4861e835
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-05-27 17:19:20 +0000

    Tue, May 27, 2025, 10:19 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -2,36 +2,25 @@ const upButton = document.querySelector('.up-button');
 const downButton = document.querySelector('.down-button');
 const rightSide = document.querySelector('.right-side');
 const leftSide = document.querySelector('.left-side');
-const slideslength = rightSide.querySelectorAll('div').length;
+const slidesLength = rightSide.querySelectorAll('div').length;
 
-//First slide
-let currentSlide = 1;
-leftSide.style.top = `${-(slideslength - currentSlide) * 100}%`;
+let currentSlide = 0;
 
-upButton.addEventListener('click', () => { changeSlide('up'); } )
-downButton.addEventListener('click', () => { changeSlide('down'); } )
+upButton.addEventListener('click', () => { changeSlide('up'); });
+downButton.addEventListener('click', () => { changeSlide('down'); });
 
 function changeSlide(direction) {
-    const topSlide = leftSide.querySelector('.top');
-    const bottomSlide = leftSide.querySelector('.bottom');
-    const rightSlide = rightSide.querySelector('.right');
-
     if (direction === 'up') {
-        if (currentSlide >= slideslength ) {
-            currentSlide = 0; // Reset to first slide
-        }
         currentSlide++;
-        leftSide.style.top = `${-(slideslength - currentSlide) * 100}%`;
-        rightSide.style.transform = `translateY(-${(currentSlide - 1) * 100}%)`;
+        if (currentSlide >= slidesLength) {
+            currentSlide = 0;
+        }
     } else if (direction === 'down') {
-        if (currentSlide >= slideslength) {
-            currentSlide = 0; // Reset to first slide
-        }
-        if (currentSlide <= 1) {
-            currentSlide = slideslength + 1; // Reset to last slide
-        }
         currentSlide--;
-        leftSide.style.top = `${-(slideslength - currentSlide) * 100}%`;
-        rightSide.style.transform = `translateY(-${(currentSlide - 1) * 100}%)`;
+        if (currentSlide < 0) {
+            currentSlide = slidesLength - 1;
+        }
     }
+    rightSide.style.transform = `translateY(-${currentSlide * 100}%)`;
+    leftSide.style.transform = `translateY(${currentSlide * 100}%)`;
 }
