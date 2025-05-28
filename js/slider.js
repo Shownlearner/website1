ceda8a5eb0219644c38d4898f24805c01d9f38a5
Shownlearner <xxxdakingxxxschool@gmail.com>
2025-05-28 09:47:05 -0700

    update

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,23 @@
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const rightSide = document.querySelector('.right-side');
-const leftSide = document.querySelector('.left-side');
-const slidesLength = rightSide.querySelectorAll('div').length;
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const rightSide = document.querySelector('.right-side')
+const leftSide = document.querySelector('.left-side')
+const slidesLength = rightSide.querySelectorAll('div').length
 
-let currentSlide = 0;
+//First Slide
+let currentSlide = 1 //slideLengt =4
+leftSide.style.top = `${-(slidesLength - currentSlide)*100}%`
 
-upButton.addEventListener('click', () => { changeSlide('up'); });
-downButton.addEventListener('click', () => { changeSlide('down'); });
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
 
-function changeSlide(direction) {
-    if (direction === 'up') {
-        currentSlide++;
-        if (currentSlide >= slidesLength) {
-            currentSlide = 0;
-        }
-    } else if (direction === 'down') {
-        currentSlide--;
-        if (currentSlide < 0) {
-            currentSlide = slidesLength - 1;
-        }
+function changeSlide(direction){
+    if (direction === 'up'){
+        currentSlide = (currentSlide % slidesLength) + 1;
+    } else {
+        currentSlide = (currentSlide - 1) || slidesLength;
     }
-    rightSide.style.transform = `translateY(-${currentSlide * 100}%)`;
-    leftSide.style.transform = `translateY(${currentSlide * 100}%)`;
+
+    leftSide.style.top = `${-(slidesLength - currentSlide) * 100}%`;
+    rightSide.style.top = `${-(currentSlide - 1) * 100}%`;
 }
--- js/speech.js
+++ js/speech.js
@@ -5,101 +5,135 @@ const readBtn = document.getElementById('read')
 const toggleBtn = document.getElementById('toggle')
 const closeBtn = document.getElementById('close')
 
-
 const data = [
     {
-        image:'../drink.jpg',
-        text: "I'm thirsty"
+        image: 'drink.jpg',
+        text: "I'm Thirsty"
     },
     {
-        image:'../food.jpg',
+        image: 'food.jpg',
         text: "I'm Hungry"
     },
     {
-        image:'../tired.jpg',
-        text: "I'm tired"
+        image: 'tired.jpg',
+        text: "I'm Tired"
     },
     {
-        image:'../hurt.jpg',
-        text: "I'm hurt"
+        image: 'hurt.jpg',
+        text: "I'm Hurt"
     },
     {
-        image:'../happy.jpg',
-        text: "I'm happy"
+        image: 'happy.jpg',
+        text: "I'm Happy"
     },
     {
-        image:'../angry.jpg',
+        image: 'angry.jpg',
         text: "I'm Angry"
     },
     {
-        image:'../sad.jpg',
-        text: "I'm sad"
+        image: 'sad.jpg',
+        text: "I'm Sad"
     },
     {
-        image:'../scared.jpg',
-        text: "I'm scared"
+        image: 'scared.jpg',
+        text: "I'm Scared"
     },
     {
-        image:'../outside.jpg',
-        text: "I'm outside"
+        image: 'outside.jpg',
+        text: "I want to go outside..."
     },
     {
-        image:'../home.jpg',
-        text: "I'm in your home ;)"
+        image: 'home.jpg',
+        text: "I want to go home..."
     },
     {
-        image:'../school.jpg',
-        text: "I want to go to school"
+        image: 'school.jpg',
+        text: "I want to go to school..."
     },
     {
-        image:'../grandma.jpg',
-        text: "I've got your grandma"
+        image: 'grandma.jpg',
+        text: "I want to go to grandma's..."
     }
 ]
+
 
 data.forEach(createBox)
 
 function createBox(item) {
-    const box = document.createElement('div.')
+    const box = document.createElement('div')
     box.classList.add('box')
     box.innerHTML = `
-    <img src= "${item.image}" alt="${item.text}"/>
-    <p class = "info"> ${item.text} </p>
+        <img src="${item.image}" alt="${item.text}" />
+        <p class="info"> ${item.text} </p>
     `
 
-    box.addEventListener('click', ()=> {
-        setTextMessage()
+    box.addEventListener('click', () => {
+        setTextMessage(item.text)
+        speakText()
+
+        //add active effect
+        box.classList.add('active')
+        setTimeout( () => box.classList.remove('active'), 800)
     })
 
     main.appendChild(box)
- }
+}
+
+//initialize speech synthesis
+const message = new SpeechSynthesisUtterance()
+
+//set text
+function setTextMessage(text) {
+    message.text = text
+}
+
+//speak text
+function speakText() {
+    speechSynthesis.speak(message)
+}
+
 
 //toggle text box
-toggleBtn.addEventListener('click', ()=> {
+toggleBtn.addEventListener('click', () => {
     document.getElementById('text-box').classList.toggle('show')
 })
 
-//Close box
-closeBtn.addEventListener('click', ()=> {
+//close box
+closeBtn.addEventListener('click', () => {
     document.getElementById('text-box').classList.remove('show')
 })
 
 let voices = []
 
 function getVoices() {
-    voices = speechSysthesis.getVoices()
+    voices = speechSynthesis.getVoices()
 
-    voices.forEach(voice =>{
-        const option =  document.createElemebt('option')
+    voices.forEach(voice => {
+        const option = document.createElement('option')
 
         option.value = voice.name
-        option.innerText =`${voice.name} $[voice.lang]`
-        voiceSelect.appendChild(option)
+        option.innerText = `${voice.name} ${voice.lang}`
+
+        voicesSelect.appendChild(option)
     })
 }
-
 
 // voices changed
 speechSynthesis.addEventListener('voiceschanged', getVoices)
 
+
+//change voice
+voicesSelect.addEventListener('change', setVoice)
+
+//set voice
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value)
+}
+
 getVoices()
+
+//read text button
+readBtn.addEventListener('click', () => {
+    setTextMessage(textarea.value)
+    speakText()
+})
